95bd5ba89a26a2c712c4c7a53b27299b38b4922d
joserobles <jarobles1018@gmail.com>
2024-02-23 13:53:40 -0400

    Fix typo and add favicon

--- js/game.js
+++ js/game.js
@@ -238,7 +238,7 @@ class Game {
         // Game Over Message
         const gameOverMessage = document.createElement('div');
         gameOverMessage.setAttribute('id', 'game-over-message');
-        gameOverMessage.innerHTML = '<h1>Game Over</h1><p>Micheal Scott took all your cash!</p>';
+        gameOverMessage.innerHTML = '<h1>Game Over</h1><p>Michael Scott took all your cash!</p>';
         document.body.appendChild(gameOverMessage);
 
         const gameOverImage = '<img id="game-over-img" src="img/game-over-image.png">'
